ad2f6fd561dffa70660078b48771d8c3061be5ea
Dragan Aleksic <aleksicdragan574@gmail.com>
2024-01-21 15:25:37 +0100

    Add user model
    -Create user.model.ts

---
+++ src/models/user.model.ts
@@ -0,0 +1,67 @@
+import { Document, Schema, model } from 'mongoose'
+import bcrypt from 'bcrypt'
+
+export interface IUser {
+  name: string
+  email: string
+  created: Date
+  updated: Date
+  hashed_password: string
+  salt: string
+  _password: string
+  authenticate: (password: string) => boolean
+}
+
+export interface IUserDocument extends IUser, Document {}
+
+const UserSchema = new Schema<IUserDocument>({
+  name: {
+    type: String,
+    trim: true,
+    required: [true, 'Name is required']
+  },
+  email: {
+    type: String,
+    trim: true,
+    unique: true,
+    match: [/.+@.+\..+/, 'Please fill a valid email address'],
+    required: [true, 'Email is required']
+  },
+  created: {
+    type: Date,
+    default: Date.now()
+  },
+  updated: Date,
+  hashed_password: {
+    type: String,
+    required: [true, 'Password is required']
+  },
+  salt: String
+})
+
+UserSchema.virtual('password')
+  .set(function (password) {
+    this._password = password
+    this.salt = bcrypt.genSaltSync(10)
+    this.hashed_password = bcrypt.hashSync(password, this.salt)
+  })
+  .get(function () {
+    return this._password
+  })
+
+UserSchema.methods = {
+  authenticate: function (password: string) {
+    return bcrypt.hashSync(password, this.salt) === this.hashed_password
+  }
+}
+
+UserSchema.path('hashed_password').validate(function () {
+  if (this._password && this._password.length < 6) {
+    this.invalidate('password', 'Password must be at least 6 characters.')
+  }
+  if (this.isNew && !this._password) {
+    this.invalidate('password', 'Password is required')
+  }
+}, undefined)
+
+export default model<IUserDocument>('User', UserSchema)
